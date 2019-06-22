21c674b2494b603b7845390b329b8edcf899225d
Pawel <jarrodek@gmail.com>
2019-06-22 14:17:50 -0700

    refactor: upgrading to LitElement

--- anypoint-styles.js
+++ anypoint-styles.js
@@ -1,33 +1,21 @@
-import '@polymer/iron-flex-layout/iron-flex-layout.js';
-import './colors.js';
-import './typography.js';
 import './default-theme.js';
 
-/**
-@license
-Copyright 2018 MuleSoft.
-All rights reserverd.
-*/
 /**
 The `<anypoint-styles>` component provides simple ways to use Anypoint CSS styles
 in an application. The following imports are available:
 
-1. [colors.html](https://github.com/advanced-rest-client/anypoint-styles/blob/master/colors.html):
+1. [colors.js](https://github.com/anypoint-web-components/anypoint-styles/blob/master/colors.js):
 a complete list of the colors defined in the Anypoint [palette](http://ux.mulesoft.com/#/colors)
 
-2. [typography.html](https://github.com/advanced-rest-client/anypoint-styles/blob/master/typography.html):
+2. [typography.js](https://github.com/anypoint-web-components/anypoint-styles/blob/master/typography.js):
 Anypoint [font](http://ux.mulesoft.com/#/typography) styles and sizes
 
-3. [dropdown-menu.html](https://github.com/advanced-rest-client/anypoint-styles/blob/master/dropdown-menu.html):
-Styles to style `paper-dropdown-menu` as anypoint component.
-
-4. [anypoint-theme.html](https://github.com/advanced-rest-client/anypoint-styles/blob/master/anypoint-theme.html):
+3. [anypoint-theme.js](https://github.com/anypoint-web-components/anypoint-styles/blob/master/anypoint-theme.js):
 Default theme for anypoint applications.
 
-We recommend importing each of these individual files, and using the style mixins
-available in each ones, rather than the aggregated `anypoint-styles.html` as a whole.
+We recommend importing each of these individual files, and using the style variables
+available in each ones, rather than the aggregated `anypoint-styles.js` as a whole.
 
-@group Anypoint Elements
 @pseudoElement anypoint-styles
 @demo demo/index.html
 */
--- colors.js
+++ colors.js
@@ -1,63 +1,60 @@
-import '@polymer/polymer/lib/elements/custom-style.js';
-/**
-@license
-Copyright 2018 MuleSoft.
-All rights reserverd.
-*/
-/*
-Color definitions for the Anypoint colors.
-*/
-
-const $documentContainer = document.createElement('template');
-$documentContainer.setAttribute('style', 'display: none;');
-
-$documentContainer.innerHTML = `<custom-style>
-  <style is="custom-style">
-  :root {
-    --anypoint-color-primary: #00A2DF;
+import { css } from 'lit-element';
+const style = css`
+  html {
+    --anypoint-color-primary: #00a2df;
     --anypoint-color-secondary: #506773;
-    --anypoint-color-danger: #D1344E;
-    --anypoint-color-success: #17BC65;
+    --anypoint-color-danger: #d1344e;
+    --anypoint-color-success: #17bc65;
     --anypoint-color-tertiary: #ffffff;
 
-    --anypoint-color-coreBlue1: #ABE2F5;
-    --anypoint-color-coreBlue2: #48C1ED;
-    --anypoint-color-coreBlue3: #00A2DF;
+    --anypoint-color-coreBlue1: #abe2f5;
+    --anypoint-color-coreBlue2: #48c1ed;
+    --anypoint-color-coreBlue3: #00a2df;
     --anypoint-color-coreBlue4: #087299;
     --anypoint-color-coreBlue5: #114459;
 
-    --anypoint-color-robustBlue1: #A1B1B8;
-    --anypoint-color-robustBlue2: #6B8A99;
+    --anypoint-color-robustBlue1: #a1b1b8;
+    --anypoint-color-robustBlue2: #6b8a99;
     --anypoint-color-robustBlue3: #506773;
-    --anypoint-color-robustBlue4: #32444D;
-    --anypoint-color-robustBlue5: #272F33;
+    --anypoint-color-robustBlue4: #32444d;
+    --anypoint-color-robustBlue5: #272f33;
 
-    --anypoint-color-futureGreen1: #AAF2CB;
-    --anypoint-color-futureGreen2: #33CC7A;
-    --anypoint-color-futureGreen3: #17BC65;
-    --anypoint-color-futureGreen4: #0E8C48;
-    --anypoint-color-futureGreen5: #174D30;
+    --anypoint-color-futureGreen1: #aaf2cb;
+    --anypoint-color-futureGreen2: #33cc7a;
+    --anypoint-color-futureGreen3: #17bc65;
+    --anypoint-color-futureGreen4: #0e8c48;
+    --anypoint-color-futureGreen5: #174d30;
 
-    --anypoint-color-aluminum1: #F9FAFB;
-    --anypoint-color-aluminum2: #F4F5F6;
-    --anypoint-color-aluminum3: #E8E9EA;
-    --anypoint-color-aluminum4: #CACBCC;
-    --anypoint-color-aluminum5: #989A9B;
+    --anypoint-color-aluminum1: #f9fafb;
+    --anypoint-color-aluminum2: #f4f5f6;
+    --anypoint-color-aluminum3: #e8e9ea;
+    --anypoint-color-aluminum4: #cacbcc;
+    --anypoint-color-aluminum5: #989a9b;
 
-    --anypoint-color-steel1: #6B6C6D;
-    --anypoint-color-steel2: #58595A;
-    --anypoint-color-steel3: #3A3B3C;
+    --anypoint-color-steel1: #6b6c6d;
+    --anypoint-color-steel2: #58595a;
+    --anypoint-color-steel3: #3a3b3c;
     --anypoint-color-steel4: #262728;
     --anypoint-color-steel5: #121314;
 
-    --anypoint-color-yellow3: #F2BE24;
-    --anypoint-color-viridian3: #00B49D;
-    --anypoint-color-teal3: #00B5D1;
-    --anypoint-color-navy3: #178BEA;
-    --anypoint-color-indigo3: #5E66F9;
-    --anypoint-color-violet3: #9A63F9;
-    --anypoint-color-red3: #D1344E;
+    --anypoint-color-yellow3: #f2be24;
+    --anypoint-color-viridian3: #00b49d;
+    --anypoint-color-teal3: #00b5d1;
+    --anypoint-color-navy3: #178bea;
+    --anypoint-color-indigo3: #5e66f9;
+    --anypoint-color-violet3: #9a63f9;
+    --anypoint-color-red3: #d1344e;
   }
-  </style>
-</custom-style>`;
-document.head.appendChild($documentContainer.content);
+`;
+
+try {
+  document.adoptedStyleSheets = document.adoptedStyleSheets.concat(style.styleSheet);
+} catch (_) {
+  /* istanbul ignore next */
+  {
+    const s = document.createElement('style');
+    s.type = 'text/css';
+    s.innerHTML = style.cssText;
+    document.getElementsByTagName('head')[0].appendChild(s);
+  }
+}
